6ff0f8aef6688de87732760fd98aa1c13bb33c45
aiktnch <aika073009150314@icloud.com>
2024-06-26 05:42:38 +0900

    css導入

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,19 +3,20 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
+        <link rel="stylesheet" href="{{ asset('css/index.css') }}?v={{ time() }}">
     </head>
-    <body>
+    <body style="background-color: #D9E5FF;">
         <form action="/posts" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="title">
-                <h2>タイトル</h2>
+                <h2 style="color: #4B0082">タイトル</h2>
                 <input type="hidden" name="post[user_id]" value="{{ Auth::id() }}"/>
                 <input type="text" name="post[title]" placeholder="タイトル"/>
                 <input type="hidden" name="post[category_id]" value="1"/>
                 <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="category">
-                <h2>カテゴリー</h2>
+                <h2 style="color: #4B0082">カテゴリー</h2>
                 <select name="post[category_id]">
                     @foreach($categories as $category)
                         <option value="{{ $category->id }}">{{ $category->name }}</option>
@@ -23,7 +24,7 @@
                 </select>
             </div>
             <div class="summary">
-                <h2>概要</h2>
+                <h2 style="color: #4B0082">概要</h2>
                 <textarea name="summary_body" placeholder="作品の概要"></textarea>
                 <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
@@ -31,7 +32,7 @@
                 <input type="file" name="summary_image">
                 
             <div class="ingredient">
-                <h2>材料</h2>
+                <h2 style="color: #4B0082">材料</h2>
                 <textarea name="ingredient_body" placeholder="使用したものを書いてください"></textarea>
                 <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
@@ -39,7 +40,7 @@
                 <input type="file" name="ingredient_image">
             
             <div class="body">
-                <h2>編み図</h2>
+                <h2 style="color: #4B0082">編み図</h2>
                 <textarea name="pattern_body" placeholder="編み図について、追記したい場合は書いてください"></textarea>
                 <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,15 +5,21 @@
     <x-slot name="header">
         トップページ
     </x-slot>
-    <h1>Blog Name</h1>
-    <a href='/posts/create'>create</a>
-    <div class='bg-red-300'>
+    <h1 class="text-center" style="font-size: 50px;color: #4B0082;line-height:150px">編み物アプリ</h1>
+    <a  href='/posts/create' style="background-color:#FF88FF;font-size: 20px;color: white;border: solid 3px white;padding: 3px;line-height:100px">★投稿する★</a>
+    
+    <div style="background-color: #FFEEFF;">
         @foreach ($posts as $post)
-            <div class='post'>
-                <h2 class='title'>
+            <div class='post' style="line-height :2; border: dotted 1px #4B0082;">
+                <h2 class='title' style="color: black font-size: 10px">
                     <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
                 </h2>
-                <p class='body'>{{ $post->summary->body }}</p>
+                <p class='body' style="color: gray">{{ $post->summary->body }}</p>
+                    <div class="summary-images" style="height:350px;width:400px">
+            @foreach($post->summary->summary_images as $img)
+                <img src="{{ $img->image_url }}" alt="画像が読み込めません。"/>
+            @endforeach
+        </div>
             </div>
         @endforeach
     </div>
